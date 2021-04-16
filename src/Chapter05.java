6ddc6ea13fa9eee28b89f3debf7d6bee8ff6afa6
Yuta <yuta.shogai@gmail.com>
2021-04-16 23:23:24 +0900

    Java演習初級 第5章終了

---
+++ src/Chapter05.java
@@ -0,0 +1,28 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 12345678901234L;
+		double num2 = 0.12345678901234567890;
+		boolean boo1 = false;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(boo1);
+
+		Float num3 = 0.12345678901234f;
+		Integer num4 = 1234567890;
+		Character char1 = 'ん';
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(char1);
+
+		int[] array1 = new int[5];
+		array1[0] = 33488;
+		array1[1] = 13256;
+		array1[2] = 63849;
+		array1[3] = 45628;
+		array1[4] = 31562;
+		int int1 = array1[3];
+		System.out.println(int1);
+		System.out.println(array1[4]);
+	}
+}
