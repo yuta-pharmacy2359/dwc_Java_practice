dd5874ed8b8538f1847cc83a48c286f294ce2f8a
Yuta <yuta.shogai@gmail.com>
2021-04-16 17:45:56 +0900

    Java演習初級 第4章終了

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "Michael Hartl";
+		System.out.println(str1);
+
+		str1 = "Time Is Money!";
+		System.out.println(str1);
+
+		final int num1 = 18;
+		System.out.println(num1);
+	}
+}
---
+++ src/Variable02.java
@@ -0,0 +1,11 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+
+		//エラー
+		//MIN_NUMBER = 25;
+		//ystem.out.println(MIN_NUMBER);
+	}
+}
